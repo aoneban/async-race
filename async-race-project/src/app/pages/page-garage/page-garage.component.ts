993b9f7924357f0ec2f3905cef1ae157c88fe7bf
aoneban <torgregion1@gmail.com>
2024-11-09 21:59:43 +0100

    feat: add cars to the project

--- async-race-project/src/app/pages/page-garage/page-garage.component.ts
+++ async-race-project/src/app/pages/page-garage/page-garage.component.ts
@@ -1,15 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { InputCreateComponent } from '../../components/input-create/input-create.component';
 import { InputUpdateComponent } from '../../components/input-update/input-update.component';
 
+interface Car {
+  id: number;
+  name: string;
+  color: string;
+}
+
 @Component({
   selector: 'app-page-garage',
   standalone: true,
-  imports: [RouterModule, InputCreateComponent, InputUpdateComponent],
+  imports: [CommonModule, RouterModule, InputCreateComponent, InputUpdateComponent],
   templateUrl: './page-garage.component.html',
-  styleUrl: './page-garage.component.css'
+  styleUrl: './page-garage.component.css',
 })
-export class PageGarageComponent {
+export class PageGarageComponent implements OnInit {
   title = 'async-race-app';
+  getUrl = 'http://localhost:3000';
+  path = {
+    garage: '/garage',
+    engine: '/engine',
+  };
+  cars: Car[] = [];
+
+  async getCars(): Promise<Car[]> {
+    const result = await fetch(`${this.getUrl}${this.path.garage}`);
+    const data = await result.json();
+    return data as Car[];
+  }
+
+  async ngOnInit() {
+    this.cars = await this.getCars();
+  }
 }
